122dfa004a169057e4c278c7de9568159c8f323f
W-Jessica <54887332+W-Jessica@users.noreply.github.com>
2020-04-30 16:53:27 +0800

    12

---
+++ Interpreter/Client.java
@@ -0,0 +1,26 @@
+
+
+public class Client {
+	public static void main(String[] args) {
+		Expression isMale=getMaleExpression();
+			Expression isMarriedWoman=getMarriedWomanExpression();
+			
+			System.out.println("John is male?"+isMale.interpret("Male John"));
+			System.out.println("Julie is a married women?"+
+			isMarriedWoman.interpret("Married Julie"));
+	}
+	
+	//Robert和John是男性
+	public static Expression getMaleExpression() {
+		Expression robert=new TerminalExpression("Robert");
+		Expression john=new TerminalExpression("John");
+		return new OrExpression(robert,john);
+	}
+	
+	//Julie是一个已婚的女性
+	public static Expression getMarriedWomanExpression() {
+		Expression julie=new TerminalExpression("Julie");
+		Expression married=new TerminalExpression("Married");
+		return new AndExpression(julie,married);
+	}
+}
